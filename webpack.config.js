045420e9ed21d772b146162aaf97e3a9c5fe3534
Tim Donahue <tdonahue@donahuetechnology.com>
2017-12-12 15:39:23 -0500

    Finished 9.77, start react-router config

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,8 @@ module.exports = {
     },
     devtool: 'cheap-module-source-map',
     devServer: {
-        contentBase: path.join(__dirname, 'public')
+        contentBase: path.join(__dirname, 'public'),
+        historyApiFallback: true
     }
 };
 
