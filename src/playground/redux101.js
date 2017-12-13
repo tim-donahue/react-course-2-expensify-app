a675764a99591aa68e0fb152b4c71b2c2fd213fb
Tim Donahue <tdonahue@donahuetechnology.com>
2017-12-13 15:25:32 -0500

    Finished 10.90, refactoring and organizing
    Created Action Generators

--- src/playground/redux101.js
+++ src/playground/redux101.js
@@ -1,16 +1,32 @@
 import { createStore } from 'redux';
 
+// Action generators
+const incrementCount = ( {incrementBy = 1} = {} ) => ({
+    type: 'INCREMENT',
+    incrementBy
+});
+
+const decrementCount = ( {decrementBy = 1} = {} ) => ({
+    type: 'DECREMENT',
+    decrementBy
+});
+const resetCount = () => ({
+    type: 'RESET'
+});
+const setCount = ( {count} ) => ({
+    type: 'SET',
+    count
+});
+
 const store = createStore((state = { count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
-            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
             return {
-                count: state.count + incrementBy
+                count: state.count + action.incrementBy
             };
         case 'DECREMENT':
-            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
             return {
-                count: state.count - decrementBy
+                count: state.count - action.decrementBy
             };
         case 'RESET':
             return {
@@ -29,26 +45,20 @@ const unsubscribe = store.subscribe(() => {
     console.log(store.getState());
 });
 
-store.dispatch({
-    type: 'INCREMENT',
-    incrementBy: 5
-});
-store.dispatch({
-    type: 'INCREMENT'
-});
-store.dispatch({
-    type: 'RESET'
-});
-store.dispatch({
-    type: 'DECREMENT'
-});
-store.dispatch({
-    type: 'DECREMENT',
-    decrementBy: 2
-});
+// store.dispatch({
+//     type: 'INCREMENT',
+//     incrementBy: 5
+// });
 
-store.dispatch({
-    type: 'SET',
-    count: 101
-});
+store.dispatch(incrementCount( { incrementBy: 5 } ));
+
+store.dispatch(incrementCount());
+
+store.dispatch(resetCount());
+
+store.dispatch(decrementCount());
+
+store.dispatch(decrementCount({decrementBy: 10}));
+
+store.dispatch(setCount({count: 201}));
 
