5d01b87aa96dfebb5933ac3a6a9dfd1635da33dc
Tim Donahue <tdonahue@donahuetechnology.com>
2017-12-13 22:13:21 -0500

    Finished 10.97, sorting redux data

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -146,6 +146,12 @@ const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
         const textMatch = text.length === 0 || expense.description.toLowerCase().includes(text.toLowerCase());
 
         return  startDateMatch && endDateMatch && textMatch;
+    }).sort( (a, b) => {
+        if (sortBy === 'date') {
+            return a.createdAt < b.createdAt ? 1 : -1
+        } else if (sortBy === 'amount') {
+            return a.amount < b.amount ? 1 : -1
+        }
     });
 };
 
@@ -166,16 +172,16 @@ store.subscribe(() => {
     console.log(visibleExpenses);
 });
 
-const expenesOne = store.dispatch(addExpense({description: 'rent', amount: 10000, createdAt: 1000}));
-const expenseTwo = store.dispatch(addExpense({description: 'coffee', amount: 200, createdAt: -1000}));
+const expenesOne = store.dispatch(addExpense({description: 'rent', amount: 100, createdAt: 1000}));
+const expenseTwo = store.dispatch(addExpense({description: 'coffee', amount: 200, createdAt: 10000}));
 //
 // store.dispatch(removeExpense({ id: expenesOne.expense.id }));
 // store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
 //
-store.dispatch(setTextFilter('rent'));
-store.dispatch(setTextFilter());
+// store.dispatch(setTextFilter('rent'));
+// store.dispatch(setTextFilter());
 //
-// store.dispatch(sortByAmount());
+store.dispatch(sortByAmount());
 // store.dispatch(sortByDate());
 
 // store.dispatch(setStartDate(0));
