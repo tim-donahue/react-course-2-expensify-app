de3a4223de40c53ac16feef41c1a11c880e651ca
Tim Donahue <tdonahue@donahuetechnology.com>
2017-12-20 18:58:28 -0500

    Start Webpack configuration for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,38 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
+module.exports = (env, argv) => {
+  const isProduction = env === 'production';
+
+  return (
+    {
+      entry: './src/app.js',
+      output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
-    },
-    module: {
+      },
+      module: {
         rules: [
-            {
-                loader: 'babel-loader',
-                test: /\.js$/,
-                exclude: /node_modules/
-            },
-            {
-                test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
+          {
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+          },
+          {
+            test: /\.s?css$/,
+            use: [
+              'style-loader',
+              'css-loader',
+              'sass-loader'
+            ]
+          }
         ]
-    },
-    devtool: 'cheap-module-source-map',
-    devServer: {
+      },
+      devtool: isProduction ? 'source-map' : 'cheap-module-source-map',
+      devServer: {
         contentBase: path.join(__dirname, 'public'),
         historyApiFallback: true
+      }
     }
-};
 
+  );
+};
